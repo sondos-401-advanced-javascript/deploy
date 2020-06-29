c384bb26d7541ba97e592472b92e037b5d2559c7
sondoshassan <sondos199628@gmail.com>
2020-06-29 14:44:33 +0300

    Updates

---
+++ precache-manifest.990a6a7a2212b130555337e27c9c79b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756266cc72560f6c2ded9aed45449d41",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "cf7b7bf45cefb42814ac",
+    "url": "/deploy/static/css/main.29dc62a2.chunk.css"
+  },
+  {
+    "revision": "715ce0b1b07f48a73a0a",
+    "url": "/deploy/static/js/2.a0310612.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.a0310612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7b7bf45cefb42814ac",
+    "url": "/deploy/static/js/main.c4412d7b.chunk.js"
+  },
+  {
+    "revision": "68947b432cbaa31091e1",
+    "url": "/deploy/static/js/runtime-main.c02eb37a.js"
+  }
+]);
